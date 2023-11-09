2874bfa1f9b41516e9d4948e9bc848cd0c621bc6
AgustinGonzalez <agustin_.gonzalez_@hotmail.com>
2023-11-09 17:07:59 -0300

    favicon

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -3,10 +3,10 @@ import NavLinksContainer from "./NavLinksContainer";
 
 const Navbar = () => {
 	return (
-		<nav className="py-4 px-5 border-0 border-b-[1px] border-black">
+		<nav className="p-5 border-0 border-b-[1px] border-black">
 			<div className="container mx-auto flex justify-between">
 				<div className="flex items-center">
-					<Image src="/icon.png" alt="icon" width={96} height={60} priority={true} />
+					<Image src="/icon.png" alt="icon" width={50} height={50} priority={true} style={{ width: "auto", height: "auto" }} />
 				</div>
 				<NavLinksContainer />
 			</div>
