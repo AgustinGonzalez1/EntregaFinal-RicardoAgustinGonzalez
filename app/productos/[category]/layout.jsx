3b11713a931f540bd1d4fe6d3bc2c1194fc1b1cd
AgustinGonzalez <agustin_.gonzalez_@hotmail.com>
2023-11-09 19:13:28 -0300

    nav products

--- app/productos/[category]/layout.jsx
+++ app/productos/[category]/layout.jsx
@@ -6,30 +6,30 @@ const Layout = ({ children }) => {
 	const pathName = usePathname();
 	return (
 		<section>
-			<nav>
-				<ul>
+			<nav className="container mx-auto p-2">
+				<ul className="flex justify-center gap-5">
 					<li className="relative">
-						<Link href={"/productos/todos"} className={`sobre ${pathName === "/productos/todos" && "text-red-600"}`}>
+						<Link href={"/productos/todos"} className={`sobre ${pathName === "/productos/todos" && "activeSobre"}`}>
 							Todos
 						</Link>
 					</li>
 					<li className="relative">
-						<Link href={"/productos/aros"} className={`sobre ${pathName === "/productos/aros" && "text-red-600"}`}>
+						<Link href={"/productos/aros"} className={`sobre ${pathName === "/productos/aros" && "activeSobre"}`}>
 							Aros
 						</Link>
 					</li>
 					<li className="relative">
-						<Link href={"/productos/anillos"} className={`sobre ${pathName === "/productos/anillos" && "text-red-600"}`}>
+						<Link href={"/productos/anillos"} className={`sobre ${pathName === "/productos/anillos" && "activeSobre"}`}>
 							Anillos
 						</Link>
 					</li>
 					<li className="relative">
-						<Link href={"/productos/collares"} className={`sobre ${pathName === "/productos/collares" && "text-red-600"}`}>
+						<Link href={"/productos/collares"} className={`sobre ${pathName === "/productos/collares" && "activeSobre"}`}>
 							Collares
 						</Link>
 					</li>
 					<li className="relative">
-						<Link href={"/productos/pulseras"} className={`sobre ${pathName === "/productos/pulseras" && "text-red-600"}`}>
+						<Link href={"/productos/pulseras"} className={`sobre ${pathName === "/productos/pulseras" && "activeSobre"}`}>
 							Pulseras
 						</Link>
 					</li>
