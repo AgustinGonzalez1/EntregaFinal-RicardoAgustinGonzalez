b6ea9363964d8359392ee2e15bd0d07197f1fb2d
AgustinGonzalez <agustin_.gonzalez_@hotmail.com>
2023-11-15 17:40:15 -0300

    vista home1

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,20 @@
+import VistaHome from "@/components/home/VistaHome";
+
 export const metadata = {
 	title: "CORONA - INICIO",
 };
 
 export default async function Home() {
 	return (
-		<main className="flex h-[calc(100vh-75px)] flex-col items-center justify-between">
+		<main className="flex h-[calc(100vh-75px)] flex-col items-center justify-between pt-5">
 			<h1 className="text-4xl font-bold">CORONA</h1>
+			<section className="grid sm:grid-cols-2 gap-4 grid-cols-1">
+				<VistaHome image={"/home/anillos.jpeg"} title={"anillos"} />
+				<VistaHome image={"/home/aros.jpeg"} title={"aros"} />
+				<VistaHome image={"/home/collares.jpeg"} title={"collares"} />
+				<VistaHome image={"/home/pulseras.jpeg"} title={"pulseras"} />
+			</section>
+			<span></span>
 		</main>
 	);
 }
---
+++ components/home/vistaHome.jsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+const VistaHome = ({ image, title }) => {
+	return (
+		<article className="md:w-96 w-64 h-64 overflow-hidden">
+			<div className="relative w-full h-full hover:scale-110 duration-300 ease-in-out">
+				<Image priority fill src={image} alt={title} className="object-cover " sizes="100%, 100%" />
+				<span className="absolute top-0 left-0 w-full h-full bg-black opacity-50 flex justify-center items-center">
+					<p className="text-white text-xl font-bold capitalize">{title}</p>
+				</span>
+			</div>
+		</article>
+	);
+};
+export default VistaHome;
