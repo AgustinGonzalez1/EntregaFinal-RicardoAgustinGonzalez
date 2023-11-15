a0b483a4137e860dc6dc9dca2c9b3311d4803b2f
AgustinGonzalez <agustin_.gonzalez_@hotmail.com>
2023-11-15 17:57:20 -0300

    vista home.

--- app/page.jsx
+++ app/page.jsx
@@ -7,14 +7,14 @@ export const metadata = {
 export default async function Home() {
 	return (
 		<main className="flex h-[calc(100vh-75px)] flex-col items-center justify-between">
-			<h1 className="text-4xl font-bold mb-5">CORONA</h1>
-			<section className="grid sm:grid-cols-2 gap-4 grid-cols-1">
+			<h1 className="text-4xl font-bold">CORONA</h1>
+			<section className="grid sm:grid-cols-2 gap-4 my-5 grid-cols-1">
 				<SectionHome image={"/home/anillos.jpeg"} title={"anillos"} />
 				<SectionHome image={"/home/aros.jpeg"} title={"aros"} />
 				<SectionHome image={"/home/collares.jpeg"} title={"collares"} />
 				<SectionHome image={"/home/pulseras.jpeg"} title={"pulseras"} />
 			</section>
-			<span className="mt-5"></span>
+			<span></span>
 		</main>
 	);
 }
