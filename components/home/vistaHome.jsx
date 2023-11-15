0ec6756c764c6d1342953d35c393ac0a0c119922
AgustinGonzalez <agustin_.gonzalez_@hotmail.com>
2023-11-15 17:46:18 -0300

    vista home correccion

--- components/home/vistaHome.jsx
+++ components/home/vistaHome.jsx
@@ -6,7 +6,7 @@ const VistaHome = ({ image, title }) => {
 			<div className="relative w-full h-full hover:scale-110 duration-300 ease-in-out">
 				<Image priority fill src={image} alt={title} className="object-cover " sizes="100%, 100%" />
 				<span className="absolute top-0 left-0 w-full h-full bg-black opacity-50 flex justify-center items-center">
-					<p className="text-white text-xl font-bold capitalize">{title}</p>
+					<p className="text-white text-2xl font-bold capitalize">{title}</p>
 				</span>
 			</div>
 		</article>
