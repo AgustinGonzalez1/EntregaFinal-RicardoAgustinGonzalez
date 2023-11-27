46a86bdeea18ca80022ed268cd6629b8c8355868
AgustinGonzalez <agustin_.gonzalez_@hotmail.com>
2023-11-27 18:05:44 -0300

    firebase1

--- firebase/config.js
+++ firebase/config.js
@@ -1,17 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import dotenv from "dotenv";
+dotenv.config();
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: "AIzaSyCiG04aHw2IorKnJ8iiGpvX2LWPstg-ETA",
-	authDomain: "corona-coder.firebaseapp.com",
-	projectId: "corona-coder",
-	storageBucket: "corona-coder.appspot.com",
-	messagingSenderId: "1084169737257",
-	appId: "1:1084169737257:web:86e7b52952ec22acadf838",
+	apiKey: process.env.API_KEY,
+	authDomain: process.env.AUTH_DOMAIN,
+	projectId: process.env.PROJECT_ID,
+	storageBucket: process.env.STORAGE_BUCKET,
+	messagingSenderId: process.env.MESSAGING_SENDER_ID,
+	appId: process.env.APP_ID,
 };
 
 // Initialize Firebase
