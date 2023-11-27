7b07f8a0cd5ce7e3498377f67129e9b0a3e78936
AgustinGonzalez <agustin_.gonzalez_@hotmail.com>
2023-11-27 19:03:40 -0300

    solution

--- app/api/detail/[slug]/product.js
+++ app/api/detail/[slug]/product.js
@@ -1,6 +1,8 @@
 export const getProduct = async (slug) => {
 	try {
-		const product = await fetch(`http://localhost:3000/api/detail/${slug}`, { cache: "no-cache" }).then((r) => r.json());
+		const product = await fetch(`${process.env.URL_FETCH || "http://localhost:3000"}/api/detail/${slug}`, {
+			cache: "no-cache",
+		}).then((r) => r.json());
 
 		return product;
 	} catch (err) {
--- app/api/productos/[category]/route.js
+++ app/api/productos/[category]/route.js
@@ -1,11 +1,10 @@
 import { NextResponse } from "next/server";
 import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from "@/firebase/config";
-import { mockData } from "@/data/products";
 
 export async function GET(request, { params }) {
 	try {
-		/* 		const { category } = params;
+		const { category } = params;
 
 		const productsRef = collection(db, "productos");
 
@@ -13,19 +12,13 @@ export async function GET(request, { params }) {
 
 		const querySnapshot = await getDocs(q);
 
-		const docs = querySnapshot.docs.map((doc) => doc.data()); */
+		const docs = querySnapshot.docs.map((doc) => doc.data());
 
 		//Para agregar los productos a la base de datos
 		/* 	mockData.forEach((product) => {
 		const docRef = doc(db, "productos", product.slug);
 		setDoc(docRef, product);
 	}); */
-
-		const { category } = params;
-
-		const productsFiltered = category === "todos" ? mockData : mockData.filter((item) => item.category === category);
-
-		return NextResponse.json(productsFiltered);
 
 		return NextResponse.json(docs);
 	} catch (err) {
