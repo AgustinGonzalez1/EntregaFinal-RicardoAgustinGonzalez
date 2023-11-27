c209880f06daaf36f15d21fd77552bc47fdc4bd6
AgustinGonzalez <agustin_.gonzalez_@hotmail.com>
2023-11-27 18:35:49 -0300

    firebase4

--- app/api/detail/[slug]/route.js
+++ app/api/detail/[slug]/route.js
@@ -3,12 +3,16 @@ import { doc, getDoc } from "firebase/firestore";
 import { db } from "@/firebase/config";
 
 export async function GET(request, { params }) {
-	const { slug } = params;
+	try {
+		const { slug } = params;
 
-	const docRef = doc(db, "productos", slug);
-	const docSnap = await getDoc(docRef);
+		const docRef = doc(db, "productos", slug);
+		const docSnap = await getDoc(docRef);
 
-	const product = docSnap.data();
+		const product = docSnap.data();
 
-	return NextResponse.json(product);
+		return NextResponse.json(product);
+	} catch (err) {
+		console.log(err);
+	}
 }
--- app/api/productos/[category]/route.js
+++ app/api/productos/[category]/route.js
@@ -3,21 +3,25 @@ import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from "@/firebase/config";
 
 export async function GET(request, { params }) {
-	const { category } = params;
+	try {
+		const { category } = params;
 
-	const productsRef = collection(db, "productos");
+		const productsRef = collection(db, "productos");
 
-	const q = category === "todos" ? query(productsRef) : query(productsRef, where("category", "==", category));
+		const q = category === "todos" ? query(productsRef) : query(productsRef, where("category", "==", category));
 
-	const querySnapshot = await getDocs(q);
+		const querySnapshot = await getDocs(q);
 
-	const docs = querySnapshot.docs.map((doc) => doc.data());
+		const docs = querySnapshot.docs.map((doc) => doc.data());
 
-	//Para agregar los productos a la base de datos
-	/* 	mockData.forEach((product) => {
+		//Para agregar los productos a la base de datos
+		/* 	mockData.forEach((product) => {
 		const docRef = doc(db, "productos", product.slug);
 		setDoc(docRef, product);
 	}); */
 
-	return NextResponse.json(docs);
+		return NextResponse.json(docs);
+	} catch (err) {
+		console.log(err);
+	}
 }
