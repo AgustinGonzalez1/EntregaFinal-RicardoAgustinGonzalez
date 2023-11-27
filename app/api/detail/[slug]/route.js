f8e97a3f13b86cd3f103c84c425eceb54698f6df
AgustinGonzalez <agustin_.gonzalez_@hotmail.com>
2023-11-27 18:52:57 -0300

    sin firebase

--- app/api/detail/[slug]/route.js
+++ app/api/detail/[slug]/route.js
@@ -1,15 +1,22 @@
 import { NextResponse } from "next/server";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "@/firebase/config";
+import { mockData } from "@/data/products";
 
 export async function GET(request, { params }) {
 	try {
-		const { slug } = params;
+		/* 		const { slug } = params;
 
 		const docRef = doc(db, "productos", slug);
 		const docSnap = await getDoc(docRef);
 
 		const product = docSnap.data();
+
+		return NextResponse.json(product); */
+
+		const { slug } = params;
+
+		const product = mockData.find((product) => product.slug === slug);
 
 		return NextResponse.json(product);
 	} catch (err) {
