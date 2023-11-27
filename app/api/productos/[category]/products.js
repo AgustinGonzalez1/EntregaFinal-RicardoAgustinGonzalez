bfaed683c1fd8a8725a4b4b5a42ac2aab5e7739b
AgustinGonzalez <agustin_.gonzalez_@hotmail.com>
2023-11-27 19:06:18 -0300

    solution

--- app/api/productos/[category]/products.js
+++ app/api/productos/[category]/products.js
@@ -1,6 +1,8 @@
 export const getProducts = async (category) => {
 	try {
-		const data = await fetch(`http://localhost:3000/api/productos/${category}`, { cache: "no-cache" }).then((r) => r.json());
+		const data = await fetch(`${process.env.URL_FETCH || "http://localhost:3000"}/api/productos/${category}`, {
+			cache: "no-cache",
+		}).then((r) => r.json());
 
 		return data;
 	} catch (err) {
