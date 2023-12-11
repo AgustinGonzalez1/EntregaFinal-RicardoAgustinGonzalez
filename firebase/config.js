3533b2edfef02c1df2b3440ad8192e885ad13d4d
Agustin Gonzalez <agustin_.gonzalez_@hotmail.com>
2023-12-11 15:47:42 -0300

    config firebase

--- firebase/config.js
+++ firebase/config.js
@@ -1,4 +1,3 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
@@ -6,7 +5,7 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import dotenv from 'dotenv';
 dotenv.config();
 
-// Your web app's Firebase configuration
+// Config FIrebase
 const firebaseConfig = {
   apiKey: 'AIzaSyCiG04aHw2IorKnJ8iiGpvX2LWPstg-ETA',
   authDomain: 'corona-coder.firebaseapp.com',
@@ -17,7 +16,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
